0686c3a523492d51039ec04d26d381d2c265ac7b
Ulrich Schmidt <ulrich1992@web.de>
2016-11-19 23:55:14 +0100

    cleanupcontroller

--- src/content/modules/cleanup/controller/cleanup_controller.php
+++ src/content/modules/cleanup/controller/cleanup_controller.php
@@ -27,4 +27,33 @@ ORDER BY data_length DESC;";
 			Database::query ( $table );
 		}
 	}
+	/*
+	 * returns the size of all files in log folder in MB
+	 */
+	public function canCleanLogDir() {
+		return ($this->getDirSize () > 0);
+	}
+	public function getLogDirSize() {
+		return round ( $this->getDirSize ( ULICMS_ROOT . "/content/log" ) / 1024 / 1024, 2 );
+	}
+	private function getDirSize($dir_name) {
+		$dir_size = 0;
+		if (is_dir ( $dir_name )) {
+			if ($dh = opendir ( $dir_name )) {
+				while ( ($file = readdir ( $dh )) !== false ) {
+					if ($file != "." && $file != "..") {
+						if (is_file ( $dir_name . " / " . $file )) {
+							$dir_size += filesize ( $dir_name . " / " . $file );
+						}
+						/* check for any new directory inside this directory */
+						if (is_dir ( $dir_name . " / " . $file )) {
+							$dir_size += get_dir_size ( $dir_name . " / " . $file );
+						}
+					}
+				}
+			}
+		}
+		closedir ( $dh );
+		return $dir_size;
+	}
 }
