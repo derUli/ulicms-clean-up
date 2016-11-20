8513d967b59cea3eb5188e89deba34da062e8501
Ulrich Schmidt <ulrich1992@web.de>
2016-11-20 14:49:09 +0100

    sprachdatei hinzugefügt

--- src/content/modules/cleanup/lang/de.php
+++ src/content/modules/cleanup/lang/de.php
@@ -1,3 +1,5 @@
 <?php
 define ( "TRANSLATION_OBJECT", "Objekt" );
 define ( "TRANSLATION_CLEAN", "Bereinigen" );
+define ( "TRANSLATION_TRUNCATE_TABLE_X", "Leere Tabelle %x" );
+define ( "TRANSLATION_TRUNCATE_LOG_FILES", "Lösche Log Dateien" );
--- src/content/modules/cleanup/lang/en.php
+++ src/content/modules/cleanup/lang/en.php
@@ -1,3 +1,5 @@
 <?php
 define ( "TRANSLATION_OBJECT", "Object" );
 define ( "TRANSLATION_CLEAN", "Clean" );
+define ( "TRANSLATION_TRUNCATE_TABLE_X", "Truncate Table %x" );
+define ( "TRANSLATION_TRUNCATE_LOG_FILES", "Delete log files" );
