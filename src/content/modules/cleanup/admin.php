45139dfb0927d7fd7604f9074b205f41307eaad3
Ulrich Schmidt <ulrich1992@web.de>
2016-11-20 14:32:29 +0100

    log_files werden wieder angezeigt, sofern cleanable

--- src/content/modules/cleanup/admin.php
+++ src/content/modules/cleanup/admin.php
@@ -2,9 +2,41 @@
 define ( "MODULE_ADMIN_HEADLINE", "Clean Up" );
 define ( "MODULE_ADMIN_REQUIRED_PERMISSION", "cleanup" );
 function cleanup_admin() {
+	$controller = new CleanUpController ();
+	$tables = $controller->getCleanableTables ();
+	$canCleanLogFolder = $controller->canCleanLogDir ();
 	?>
 <form action="index.php?action=cleanup" method="post">
 <?php csrf_token_html();?>
+<table class="tablesorter">
+		<thead>
+			<tr>
+				<td style="width: 50px;"></td>
+				<th><?php translate("object");?></th>
+				<th><?php translate("size");?></th>
+			</tr>
+		</thead>
+		<tbody>
+<?php foreach($tables as $table){?>
+<tr>
+				<td style="text-align: center"><input type="checkbox"
+					id="c_<?php Template::escape($table)?>" name="clean_tables[]"
+					value="<?php Template::escape($table);?>" checked></td>
+				<td><?php Template::escape($table->TABLE_NAME);?></td>
+				<td><?php Template::escape($table->size_in_mb);?> MB</td>
+			</tr>
+<?php }?>
+<?php if($canCleanLogFolder){?>
+<tr>
+				<td style="text-align: center"><input type="checkbox"
+					id="c_<?php Template::escape($table)?>" name="log_files" value="1"
+					checked></td>
+				<td>log_files</td>
+				<td><?php echo $controller->getLogDirSize();?> MB</td>
+			</tr>
+<?php }?>
+</tbody>
+	</table>
 	<p>
 		<input name="submit" type="submit" value="<?php translate("clean");?>">
 	</p>
--- src/content/modules/cleanup/controller/cleanup_controller.php
+++ src/content/modules/cleanup/controller/cleanup_controller.php
@@ -33,7 +33,7 @@ ORDER BY data_length DESC;";
 	 * returns the size of all files in log folder in MB
 	 */
 	public function canCleanLogDir() {
-		return ($this->getDirSize () > 0);
+		return ($this->getLogDirSizeInByte () > 0);
 	}
 	public function cleanLogDir() {
 		return SureRemoveDir ( ULICMS_ROOT . "/content/log", false );
@@ -41,18 +41,21 @@ ORDER BY data_length DESC;";
 	public function getLogDirSize() {
 		return round ( $this->getDirSize ( ULICMS_ROOT . "/content/log" ) / 1024 / 1024, 2 );
 	}
+	private function getLogDirSizeInByte() {
+		return ($this->getDirSize ( ULICMS_ROOT . "/content/log" ));
+	}
 	private function getDirSize($dir_name) {
 		$dir_size = 0;
 		if (is_dir ( $dir_name )) {
 			if ($dh = opendir ( $dir_name )) {
 				while ( ($file = readdir ( $dh )) !== false ) {
 					if ($file != "." && $file != "..") {
-						if (is_file ( $dir_name . " / " . $file )) {
-							$dir_size += filesize ( $dir_name . " / " . $file );
+						if (is_file ( $dir_name . "/" . $file )) {
+							$dir_size += filesize ( $dir_name . "/" . $file );
 						}
 						/* check for any new directory inside this directory */
-						if (is_dir ( $dir_name . " / " . $file )) {
-							$dir_size += get_dir_size ( $dir_name . " / " . $file );
+						if (is_dir ( $dir_name . "/" . $file )) {
+							$dir_size += $this->getDirSize ( $dir_name . "/" . $file );
 						}
 					}
 				}
