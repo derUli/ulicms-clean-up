eacdf9721f618bc1a8ac12e2099f08bb0ac60b77
Ulrich Schmidt <ulrich1992@web.de>
2016-11-20 14:35:15 +0100

    sprachdatei hinzugefügt

---
+++ src/content/modules/cleanup/lang/de.php
@@ -0,0 +1,3 @@
+<?php
+define ( "TRANSLATION_OBJECT", "Objekt" );
+define ( "TRANSLATION_CLEAN", "Bereinigen" );
---
+++ src/content/modules/cleanup/lang/en.php
@@ -0,0 +1,3 @@
+<?php
+define ( "TRANSLATION_OBJECT", "Object" );
+define ( "TRANSLATION_CLEAN", "Clean" );
