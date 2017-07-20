cd32af8c71a6af1ffaff18cff92ac4d5e566d3f6
Ulrich Schmidt <admin@deruli.de>
2017-07-20 10:40:12 +0200

    back button eingebaut

--- src/content/modules/cleanup/run_cleanup.php
+++ src/content/modules/cleanup/run_cleanup.php
@@ -1,49 +1,49 @@
 <?php
-$acl = new ACL ();
-if ($acl->hasPermission ( "cleanup" )) {
-	$controller = new CleanUpController ();
-	$mysql_optimize_available = in_array ( "mysql_optimize", getAllModules () );
-	?>
-<h1>Clean Up</h1>
+$acl = new ACL();
+if ($acl->hasPermission("cleanup")) {
+    $controller = new CleanUpController();
+    $mysql_optimize_available = in_array("mysql_optimize", getAllModules());
+    ?>
+<h1><?php translate("clean");?></h1>
 <?php
-	if (isset ( $_POST ["clean_tables"] ) and is_array ( $_POST ["clean_tables"] )) {
-		foreach ( $_POST ["clean_tables"] as $table ) {
-			$controller->cleanTable ( $table );
-			$tname = Template::getEscape ( $table );
-			$crapFiles = $controller->getAllCrapFiles ();
-			?>
+    if (isset($_POST["clean_tables"]) and is_array($_POST["clean_tables"])) {
+        foreach ($_POST["clean_tables"] as $table) {
+            $controller->cleanTable($table);
+            $tname = Template::getEscape($table);
+            $crapFiles = $controller->getAllCrapFiles();
+            ?>
 <p><?php translate("TRUNCATE_TABLE_X" , array("%x" => $tname));?></p>
 <?php fcflush();?>
 <?php }}?>
 <?php
-	if (isset ( $_POST ["log_files"] )) {
-		$controller->cleanLogDir ();
-		?>
+    if (isset($_POST["log_files"])) {
+        $controller->cleanLogDir();
+        ?>
 
 <p><?php translate("TRUNCATE_LOG_FILES");?></p>
 <?php fcflush();?>
 <?php }?>
 <?php
-	if (isset ( $_POST ["tmp_files"] )) {
-		$controller->cleanTmpDir ();
-		?>
+    if (isset($_POST["tmp_files"])) {
+        $controller->cleanTmpDir();
+        ?>
 
 <p><?php translate("TRUNCATE_TMP_FILES");?></p>
 <?php fcflush();?>
 <?php }?>
 <?php
-	if (isset ( $_POST ["cache_files"] )) {
-		$controller->cleanCacheDir ();
-		?>
+    if (isset($_POST["cache_files"])) {
+        $controller->cleanCacheDir();
+        ?>
 
 <p><?php translate("CLEAR_CACHE");?></p>
 <?php fcflush();?>
 <?php }?>
 
 <?php
-	if (isset ( $_POST ["thumbs_dir"] )) {
-		$controller->cleanThumbsDir ();
-		?>
+    if (isset($_POST["thumbs_dir"])) {
+        $controller->cleanThumbsDir();
+        ?>
 
 <p><?php translate("TRUNCATE_THUMBNAIL_FILES");?></p>
 <?php fcflush();?>
@@ -61,12 +61,12 @@ if ($acl->hasPermission ( "cleanup" )) {
 
 <?php fcflush();?>
 <?php
-	if ($mysql_optimize_available and isset ( $_POST ["optimize_db"] )) {
-		include_once getModulePath ( "mysql_optimize" ) . "mysql_optimize_lib.php";
-		$cfg = new config ();
-		db_optimize ( $cfg->db_database );
-	}
-	?>
+    if ($mysql_optimize_available and isset($_POST["optimize_db"])) {
+        include_once getModulePath("mysql_optimize") . "mysql_optimize_lib.php";
+        $cfg = new config();
+        db_optimize($cfg->db_database);
+    }
+    ?>
 
 <?php fcflush();?>
 <p><?php translate("cleaning_finished");?></p>
